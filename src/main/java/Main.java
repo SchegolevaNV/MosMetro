0855c9fe60b1f095028dc6e6e99bbb68866eab1b
schegolevanv <ariwenn15@gmail.com>
2019-11-26 22:48:25 +0300

    create classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,8 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.TreeMap;
 
 public class Main {
 
@@ -15,20 +11,16 @@ public class Main {
                 "%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D0%B9_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE" +
                 "%D0%B3%D0%BE_%D0%BC%D0%B5%D1%82%D1%80%D0%BE%D0%BF%D0%BE%D0%BB%D0%B8%D1%82%D0%B5%D0%BD%D0%B0").get();
 
-//        Element table = doc.select("style").first();
-//        String string = table.toString();
-//        string = string.substring(string.indexOf("a:hover"), string.lastIndexOf("line-15 a:hover")+41);
-//        ArrayList<String> colors = new ArrayList<>();
-//
-//        for (int i = 0; i < string.length() ; i++) {
-//            if (string.charAt(i) == '#') {
-//                colors.add(string.substring(i, i+7));
-//            }
-//        }
-//        Iterator iterator = colors.iterator();
-//        while (iterator.hasNext()){
-//            System.out.println(iterator.next());
-//        }
+        Element code = doc.select("style").first();
+        String string = code.toString();
+        string = string.substring(string.indexOf("line-1 a:hover"), string.lastIndexOf("line-15 a:hover")+41);
+        ArrayList<String> colors = new ArrayList<>();
+
+        for (int i = 0; i < string.length() ; i++) {
+            if (string.charAt(i) == '#') {
+                colors.add(string.substring(i, i+7));
+            }
+        }
     }
       //  String link = href.get(1).attr("src");
 }
