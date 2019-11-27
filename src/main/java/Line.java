ba3c6e36cedc6c6355c62cf21505f5d6513660dd
SchegolevaNV <ariwenn15@gmail.com>
2019-11-27 17:42:40 +0300

    Tries, just tries

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,48 +1,55 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Line implements Comparable<Line> {
-        private int number;
-        private String name;
-        private List<Station> stations;
-
-    public Line( int number, String name)
-        {
-            this.number = number;
-            this.name = name;
-            stations = new ArrayList<>();
-        }
-
-        public int getNumber ()
-        {
-            return number;
-        }
-
-        public String getName ()
-        {
-            return name;
-        }
-
-        public void addStation (Station station)
-        {
-            stations.add(station);
-        }
-
-        public List<Station> getStations ()
-        {
-            return stations;
-        }
-
-        @Override
-        public int compareTo (Line line)
-        {
-            return Integer.compare(number, line.getNumber());
-        }
-
-        @Override
-        public boolean equals (Object obj)
-        {
-            return compareTo((Line) obj) == 0;
-        }
-    }
+public class Line {//implements Comparable<Line> {
+//        private int number;
+//        private String name;
+//        private String color;
+//        private List<Station> stations;
+//
+//    public Line (int number, String name, String color)
+//        {
+//            this.number = number;
+//            this.name = name;
+//            this.color = color;
+//            stations = new ArrayList<>();
+//        }
+//
+//        public int getNumber ()
+//        {
+//            return number;
+//        }
+//
+//        public String getName ()
+//        {
+//            return name;
+//        }
+//
+//        public String getColor ()
+//        {
+//            return color;
+//        }
+//
+//        public void addStation (Station station)
+//        {
+//            stations.add(station);
+//        }
+//
+//        public List<Station> getStations ()
+//        {
+//            return stations;
+//        }
+//
+//        @Override
+//        public int compareTo (Line line)
+//        {
+//            return Integer.compare(number, line.getNumber());
+//        }
+//
+//        @Override
+//        public boolean equals (Object obj)
+//        {
+//            return compareTo((Line) obj) == 0;
+//        }
+//    }
 }
