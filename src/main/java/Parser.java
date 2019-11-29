a4dd9ed42d2c04583d82f14ba64867cf230df44b
schegolevanv <ariwenn15@gmail.com>
2019-11-29 08:40:12 +0300

    add background and line name parsing

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,6 +1,8 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -16,42 +18,36 @@ public class Parser
     public void parse() throws IOException
     {
         Document doc = Jsoup.connect(url).maxBodySize(0).get();
-        HashSet<String> name = new HashSet<>();
         TreeMap<String, String> lines = new TreeMap<>();
+        TreeSet<String> colors = new TreeSet<>();
 
-        for (int i = 3; i <=5; i++)
-        {
-            ArrayList<Element> table = doc.select("table").get(i).select("tr");
+        for (int i = 3; i <= 5; i++) {
+            Elements table = doc.select("table").get(i).select("tr");
 
-            for (int j = 0; j < table.size(); j++)
-            {
-                String lineNumber = table.get(j).selectFirst("td").select("span.sortkey").text();
-                System.out.println(lineNumber);
+            for (int j = 0; j < table.size(); j++) {
+                Element td = table.get(j).selectFirst("td");
+                if (td != null) {
+                    String lineNumber = td.selectFirst("span.sortkey").text();
+                    String color = td.select("[style^=background:#]").attr("style").replaceAll("background:", "");
+                    String lineName = td.select("span[title$=линия]").attr("title");
+                    if (lineName.isEmpty()) {
+                        lineName = td.select("span[title]").attr("title");
+                    }
+                    lines.put(lineNumber, lineName);
+                    colors.add(color);
+                }
             }
-
-//            ArrayList<Element> lineName = doc.select("table").get(i).select("span[title$=линия]");
-//
-//            for (int j = 0; j < lineName.size(); j++)
-//            {
-//                name.add(lineName.get(j).attr("title"));
-//                //lines.put(lineNumber.get(j).text(),lineName.get(j).attr("title"));
-//            }
-//
-//            if (lineName.isEmpty())
-//            {
-//                lineName = doc.select("table").get(i).select("span[title]");
-//                lines.put(lineNumber.get(0).text(),lineName.get(0).attr("title"));
-//            }
-//        }
-//
-//        Iterator<String> i = name.iterator();
-//        while (i.hasNext())
-//            System.out.println(i.next());
-//
-//        for (Map.Entry<String, String> entry : lines.entrySet())
-//        {
-//        System.out.println("Key: " + entry.getKey() + ". Value: " + entry.getValue());
         }
+        for (Map.Entry<String, String> entry : lines.entrySet())
+        {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        Iterator iterator = colors.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
 
     }
 }
